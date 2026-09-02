0822260b00b37a8d757cb0ecff909b1e8a8422fa
hyeon9654 <pjoo6067@gmail.com>
2026-09-02 12:22:10 +0900

    dd

--- src/main/java/example/day04/AppStart.java
+++ src/main/java/example/day04/AppStart.java
@@ -4,5 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppStart {
-    
+    public static void main(String[] args) {
+        SpringBootApplication.run(AppStart)
+    }
 }
